dc586a515510ef39a024c5763d626c34ad116c04
Srinivas Lade <srinulade1@gmail.com>
2024-06-23 18:32:32 -0400

    Setup CI

--- tests/pyiceberg/conftest.py
+++ tests/pyiceberg/conftest.py
@@ -1,22 +1,41 @@
 import subprocess
 import re
+import os
+from pathlib import Path
 
 import pytest
 from pyiceberg.catalog.rest import RestCatalog
 
 
+base_path = str(Path(os.path.realpath(__file__)).parent.parent.parent)
+
+
+@pytest.fixture(scope="session")
+def build_binary():
+    subprocess.run(
+        ["go", "build", "."],
+        cwd=base_path,
+        check=True,
+        env={
+            "PATH": os.environ.get("PATH", ""),
+            "HOME": os.environ.get("HOME", ""),
+        },
+    )
+
+
 @pytest.fixture(scope="function")
-def catalog(tmp_path):
+def catalog(tmp_path, build_binary):
     process = subprocess.Popen(
         ["./denali", "start"],
+        cwd=base_path,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
         env={
             "DENALI_API_PORT": "0",
             "DENALI_WAREHOUSE_PATH": str(tmp_path),
             "DENALI_DATABASE_URL": ":memory:",
             "DENALI_DATABASE_DIALECT": "sqlite3",
         },
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
     )
 
     last_line = []
@@ -25,6 +44,8 @@ def catalog(tmp_path):
             continue
         process.stdout.readlines
         last_line.append(process.stdout.readline().decode("utf-8"))
+        if len(last_line) > 15:
+            raise EnvironmentError("Failed to start Denali Catalog Server")
 
     url = re.search(r"Started the Denali Catalog Server at `(?P<url>[\[\]\:\.\d]+)`", last_line[-1]).group("url")
     yield RestCatalog("rest_catalog", uri=f"http://{url}")
