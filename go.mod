d18a0041b879fe0c5dd1afae573b1e0ed4f60601
Srinivas Lade <srinulade1@gmail.com>
2024-06-23 17:26:10 -0400

    Create Temp Mode

--- go.mod
+++ go.mod
@@ -6,18 +6,19 @@ require (
 	github.com/apache/iceberg-go v0.0.0-20240529104050-60da61cc0368
 	github.com/google/uuid v1.6.0
 	github.com/gorilla/handlers v1.5.2
-	github.com/jackc/pgx/v5 v5.5.5
-	github.com/mattn/go-sqlite3 v1.14.22
+	github.com/klauspost/compress v1.17.8
 	github.com/pelletier/go-toml/v2 v2.2.2
+	github.com/pressly/goose/v3 v3.20.0
 	github.com/sethvargo/go-envconfig v1.0.3
 	github.com/urfave/cli/v2 v2.27.2
 	gocloud.dev v0.37.0
 )
 
+// Database drivers
 require (
-	github.com/mfridman/interpolate v0.0.2 // indirect
-	github.com/sethvargo/go-retry v0.2.4 // indirect
-	go.uber.org/multierr v1.11.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.1
+	github.com/jackc/pgx/v5 v5.5.5
+	github.com/mattn/go-sqlite3 v1.14.22
 )
 
 require (
@@ -46,7 +47,6 @@ require (
 	github.com/aws/smithy-go v1.20.2 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
-	github.com/go-sql-driver/mysql v1.8.1
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/snappy v0.0.4 // indirect
@@ -57,12 +57,12 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/klauspost/compress v1.17.8
+	github.com/mfridman/interpolate v0.0.2 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/pressly/goose/v3 v3.20.0
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/sethvargo/go-retry v0.2.4 // indirect
 	github.com/wolfeidau/s3iofs v1.5.2 // indirect
 	github.com/xrash/smetrics v0.0.0-20240312152122-5f08fbb34913 // indirect
 	go.opencensus.io v0.24.0 // indirect
@@ -70,6 +70,7 @@ require (
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.51.0 // indirect
 	go.opentelemetry.io/otel v1.26.0 // indirect
 	go.opentelemetry.io/otel/trace v1.26.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842 // indirect
 	golang.org/x/mod v0.17.0 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"runtime/debug"
@@ -30,11 +31,12 @@ var Version = func() string {
 	return strings.TrimSpace(uuid.New().String())
 }()
 
-func runServer(config *logic.Config) error {
+func RunServer(config *logic.Config) error {
 	// Setup by loading config and connect to database
-	s := logic.NewState()
+	s := logic.NewStateFromConfig(config)
 
 	// Create Router and API routes
+	log.Println("Creating Router and API routes")
 	mux := http.NewServeMux()
 	r := routes.Router{Mux: mux, State: s}
 	r.ConfigRoutes()
@@ -42,10 +44,20 @@ func runServer(config *logic.Config) error {
 	r.ViewRoutes()
 	r.TableRoutes()
 
+	log.Println("Middleware")
 	var handler http.Handler = mux
 	handler = handlers.LoggingHandler(os.Stdout, handler)
-	log.Printf("Starting server on port `%v`", config.Api.Port)
-	return http.ListenAndServe(fmt.Sprintf(":%v", s.Config.Api.Port), handler)
+
+	// Net Listener for Extracting Final Address
+	// If port == 0, a random available port is assigned
+	log.Println("Finding Port", config.Api.Port)
+	l, err := net.Listen("tcp", fmt.Sprintf(":%v", config.Api.Port))
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Started the Denali Catalog Server at `%v`", l.Addr().String())
+	return http.Serve(l, handler)
 }
 
 func main() {
@@ -91,21 +103,50 @@ Global Flags:
 				Usage: "Start the REST API server",
 				Flags: []cli.Flag{
 					&cli.Uint64Flag{
-						Name: "port", Aliases: []string{"p"},
+						Name:        "port",
+						Aliases:     []string{"p"},
 						Usage:       "Port to run the REST server on",
-						Value:       0,
-						DefaultText: "config file, or 8080",
+						Value:       0, // 0 means random port
+						DefaultText: "From config file, or randomly chosen if unset",
 						EnvVars:     []string{"DENALI_API_PORT"},
+					},
+					&cli.BoolFlag{
+						Name:    "temp",
+						Aliases: []string{"t"},
+						Usage:   "Run in temporary mode (in-memory database, temporary storage)",
+						Value:   false,
 					},
 				},
 				Action: func(ctx *cli.Context) error {
-					path := ctx.Path("config")
-					config := logic.LoadConfig(&path)
+					var config *logic.Config
+					log.Println("Right Path", ctx.Bool("temp"))
+					if ctx.Bool("temp") {
+						config = new(logic.Config)
+						config.Database.Url = ":memory:"
+						config.Database.Dialect = "sqlite3"
+
+						// Create temporary directory
+						path, err := os.MkdirTemp("", "denali-wh-")
+						if err != nil {
+							return fmt.Errorf("failed to create temporary directory: %v", err)
+						}
+						config.Warehouse.Path = path
+
+					} else {
+						path := ctx.Path("config")
+						config = logic.LoadConfig(&path)
+					}
+
 					if port := ctx.Uint64("port"); port != 0 {
 						config.Api.Port = port
 					}
 
-					return runServer(config)
+					err := RunServer(config)
+					if err != nil {
+						return fmt.Errorf("failed when running server: %v", err)
+					}
+
+					return nil
 				},
 			},
 		},
